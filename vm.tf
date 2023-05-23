87467479eaae0796bf0d910681468bab290ca96d
Marco Boschetti <105826129+MarcoBosc@users.noreply.github.com>
2023-05-23 16:09:19 -0300

    changes on main.tf

--- vm.tf
+++ vm.tf
@@ -7,11 +7,12 @@ resource "aws_instance" "vm" {
   ami                         = "ami-0d527b8c289b4af7f"
   instance_type               = "t2.micro"
   key_name                    = aws_key_pair.key.key_name
-  subnet_id                   = data.terraform_remote_state.vpc.outputs.subnet_id
-  vpc_security_group_ids      = [data.terraform_remote_state.vpc.outputs.security_group_id]
+  subnet_id                   = subnet-0c5c9205f1bba9214
+  vpc_security_group_ids      = [sg-0962723ccb8b0a0e8]
   associate_public_ip_address = true
 
   tags = {
     "Name" = "vm-terraform"
+    "Managed-by" = "Terraform"
   }
 }
