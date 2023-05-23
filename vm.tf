f00013d4edaa189d019087c77f77fd103be1701b
Marco Boschetti <105826129+MarcoBosc@users.noreply.github.com>
2023-05-23 16:10:38 -0300

    changes on main.tf

--- vm.tf
+++ vm.tf
@@ -7,8 +7,8 @@ resource "aws_instance" "vm" {
   ami                         = "ami-0d527b8c289b4af7f"
   instance_type               = "t2.micro"
   key_name                    = aws_key_pair.key.key_name
-  subnet_id                   = subnet-0c5c9205f1bba9214
-  vpc_security_group_ids      = [sg-0962723ccb8b0a0e8]
+  subnet_id                   = "subnet-0c5c9205f1bba9214"
+  vpc_security_group_ids      = ["sg-0962723ccb8b0a0e8"]
   associate_public_ip_address = true
 
   tags = {
